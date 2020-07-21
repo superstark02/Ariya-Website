2d378f99113410f7882d840e37fff0bacccae74a
superstark02 <superstark02@gmail.com>
2020-07-21 22:28:31 +0530

    12

--- src/Components/AppBar.js
+++ src/Components/AppBar.js
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Drawer from '@material-ui/core/Drawer';
-import { IconButton, Divider} from '@material-ui/core';
+import { IconButton, Divider } from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -20,6 +20,8 @@ import PeopleOutlineOutlinedIcon from '@material-ui/icons/PeopleOutlineOutlined'
 import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
 import LocalPhoneOutlinedIcon from '@material-ui/icons/LocalPhoneOutlined';
 import QuestionAnswerOutlinedIcon from '@material-ui/icons/QuestionAnswerOutlined';
+import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
+import ClassOutlinedIcon from '@material-ui/icons/ClassOutlined';
 
 function ElevationScroll(props) {
     const { children, window } = props;
@@ -84,35 +86,44 @@ export default function MyAppBar(props) {
             onKeyDown={toggleDrawer(anchor, false)}
         >
             <div style={{ padding: '0px 15px' }} >
-                <div style={{width:'100%',height:'50px'}} >
+                <div style={{ width: '100%', height: '50px' }} >
                     LOGO IMAGE
                 </div>
                 <List>
                     <a href='/' className='Link' ><ListItem button >
-                        <ListItemIcon><HomeOutlinedIcon/></ListItemIcon>
+                        <ListItemIcon><HomeOutlinedIcon /></ListItemIcon>
                         <ListItemText primary='Home' />
                     </ListItem></a>
 
                     <a href='/coding_courses' className='Link' ><ListItem button >
-                        <ListItemIcon><ListAltOutlinedIcon/></ListItemIcon>
+                        <ListItemIcon><ListAltOutlinedIcon /></ListItemIcon>
                         <ListItemText primary='Courses' />
                     </ListItem></a>
 
+                    <a href='/trial_classes' className='Link' ><ListItem button >
+                        <ListItemIcon><ClassOutlinedIcon /></ListItemIcon>
+                        <ListItemText primary='Trial Class' />
+                    </ListItem></a>
+
                     <a href='/coding_questions' className='Link' ><ListItem button >
-                        <ListItemIcon><QuestionAnswerOutlinedIcon/></ListItemIcon>
+                        <ListItemIcon><QuestionAnswerOutlinedIcon /></ListItemIcon>
                         <ListItemText primary='FAQs' />
                     </ListItem></a>
 
                 </List>
-                <Divider/>
+                <Divider />
                 <List>
+                    <a href='/policy_and_privacy' className='Link'><ListItem button >
+                        <ListItemIcon><AssignmentOutlinedIcon /></ListItemIcon>
+                        <ListItemText primary='Policy And Privacy' />
+                    </ListItem></a>
                     <a href='/about_us' className='Link'><ListItem button >
-                        <ListItemIcon><PeopleOutlineOutlinedIcon/></ListItemIcon>
+                        <ListItemIcon><PeopleOutlineOutlinedIcon /></ListItemIcon>
                         <ListItemText primary='About Us' />
                     </ListItem></a>
 
                     <a href='/coding_contact' className='Link'><ListItem button >
-                        <ListItemIcon><LocalPhoneOutlinedIcon/></ListItemIcon>
+                        <ListItemIcon><LocalPhoneOutlinedIcon /></ListItemIcon>
                         <ListItemText primary='Contact Us' />
                     </ListItem></a>
                 </List>
@@ -125,12 +136,18 @@ export default function MyAppBar(props) {
             <CssBaseline />
             <ElevationScroll {...props}>
                 <AppBar style={{ backgroundColor: 'rgba(255,255,255,0.3)' }} >
-                    <Toolbar style={{ backgroundColor: 'rgba(4, 191, 191,0.5)', display: 'flex', justifyContent: 'space-between',padding:"10px 50px" }} >
+                    <Toolbar style={{ backgroundColor: 'rgba(4, 191, 191,0.5)', display: 'flex', justifyContent: 'space-between', padding: "10px 50px" }} >
                         <Typography style={{ fontFamily: 'j', margin: '10px', fontSize: '30px', color: 'white' }} >
-                            <div style={{width:"100%",display:"flex",jus}} >
+                            <div style={{ width: "100%", display: "flex", justifyContent: "space-between" }} >
                                 <Link to='/' className="Link" >{"<Coding Classes/>"}</Link>
                             </div>
                         </Typography>
+                        <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer('right', true)} >
+                            <FaBars color="white" />
+                        </IconButton>
+                        <Drawer anchor={"right"} open={state['right']} onClose={toggleDrawer('right', false)}>
+                            {list("right")}
+                        </Drawer>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
